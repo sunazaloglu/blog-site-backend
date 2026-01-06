b3b9008aebe396567e67e96c088d04560e47c098
Su Naz Aloğlu <sualoglu@hotmail.com>
2026-01-06 16:33:39 +0300

    create_posts_table mig ration write

--- migrations/20260106130519_create_posts_table.js
+++ migrations/20260106130519_create_posts_table.js
@@ -1,15 +1,20 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
-};
+export async function up(knex) {
+  await knex.schema.createTable("posts", (table) => {
+    table.increments("id").primary();
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.timestamp("deleted_at").nullable();
+    table.string("title").notNullable();
+    table.text("content").notNullable();
+    table.timestamp("published_at").nullable();
+    table
+      .integer("category_id")
+      .references("id")
+      .inTable("categories")
+      .onDelete("CASCADE");
+    table.string("name").notNullable();
+  });
+}
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
-};
+export async function down(knex) {
+  await knex.schema.dropTable("categories");
+}
