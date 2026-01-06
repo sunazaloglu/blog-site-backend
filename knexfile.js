edbb463924fbe3355ccbecd722643622d3e9fe17
Su Naz Aloğlu <sualoglu@hotmail.com>
2026-01-06 16:06:09 +0300

    add migrations

--- knexfile.js
+++ knexfile.js
@@ -7,10 +7,10 @@ export default {
     client: "pg",
     connection: {
       host: process.env.DB_HOST,
-      port: DB_PORT,
-      database: DB_NAME,
-      user: DB_USER,
-      password: DB_PASSWORD,
+      port: process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
     },
     migrations: {
       directory: "./src/migrations",
