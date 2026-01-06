9a9cc64723fd212d52a7d9eac0994b3ca19fd0b4
Su Naz Aloğlu <sualoglu@hotmail.com>
2026-01-06 21:34:58 +0300

    write postModel

---
+++ src/models/postModel.ts
@@ -0,0 +1,28 @@
+import db from "../config/database.js";
+
+export const getAllPosts = async () => {
+  const query = db("posts");
+  return query.select("id", "title");
+};
+
+export const createPost = async (data: object) => {
+  return db("posts").insert({ data }).returning("*");
+};
+
+export const updatePost = async (id: number, data: object) => {
+  return db("posts")
+    .where({ id, deleted_at: null })
+    .update(data)
+    .returning("*");
+};
+
+export const deleteCategory = async (id: number) => {
+  return db("posts")
+    .where({ id, deleted_at: null })
+    .update({ deleted_at: new Date() })
+    .returning("*");
+};
+
+export const getPostById = async (id: number) => {
+  return db("posts").where({ id, deleted_at: null }).first();
+};
