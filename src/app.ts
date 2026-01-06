3ae459935069c0990336fe057564906aba05d3a0
Su Naz Aloğlu <sualoglu@hotmail.com>
2026-01-06 17:41:06 +0300

    write app.ts

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express, { type Request, type Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+
+app.get("/projectcheck", (req: Request, res: Response) => {
+  res.status(200).json({ message: "OK" });
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running on port ${PORT}");
+});
