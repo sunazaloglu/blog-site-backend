3d5bcc787705145969913c131af863672918da6d
Su Naz Aloğlu <sualoglu@hotmail.com>
2026-01-06 17:01:33 +0300

    create_comments_table mig ration write

--- migrations/20260106130547_create_comments_table.js
+++ migrations/20260106130547_create_comments_table.js
@@ -1,15 +1,17 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
-};
+export async function up(knex) {
+  await knex.schema.createTable("comments", (table) => {
+    table.increments("id").primary();
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.string("commenter_name").notNullable();
+    table.text("content").notNullable();
+    table
+      .integer("post_id")
+      .references("id")
+      .inTable("posts")
+      .onDelete("CASCADE");
+  });
+}
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
-};
+export async function down(knex) {
+  await knex.schema.dropTable("comments");
+}
