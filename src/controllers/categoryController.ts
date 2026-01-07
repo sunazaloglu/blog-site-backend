7c388a5e7a557482d3747dd4a926b375f9edf77b
Su Naz Aloğlu <sualoglu@hotmail.com>
2026-01-07 15:50:20 +0300

    change getAllCategoriesController

--- src/controllers/categoryController.ts
+++ src/controllers/categoryController.ts
@@ -12,11 +12,17 @@ export const getAllCategoriesController = async (
   res: Response
 ) => {
   try {
-    const items = await getAllCategories();
-    res.status(200).json(items);
+    const showDeleted = req.query.showDeleted?.toString() as
+      | "true"
+      | "false"
+      | "onlyDeleted"
+      | undefined;
+
+    const items = await getAllCategories(showDeleted);
+    return res.status(200).json(items);
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "Internal server error" });
+    return res.status(500).json({ message: "Internal server error" });
   }
 };
 
